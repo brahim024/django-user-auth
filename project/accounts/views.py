1a9909c82ebe81c84c24421ff2ed62384029602b
brahim024 <ifninos168@gmail.com>
2020-08-08 01:53:58 +0200

    add forms

--- project/accounts/views.py
+++ project/accounts/views.py
@@ -4,15 +4,19 @@ from .forms import SignupForm
 from django.contrib.auth import login , authenticate
 # Create your views here.
 def signup(request):
-    if request.method=='POST': ## save 
+    if request.method=='POST': ## if request = POST send data in form
         form=SignupForm(request.POST)
+        #check data 
         if form.is_valid():
             form.save()
             username=form.cleaned_data['username']
             password=form.cleaned_data['password1']
-            user=authenticate(username=username,password=password)
-            login(request,user)
-            return redirect(request,'registration/profile.html')
+            user=authenticate(username=username, password=password)
+            login (request,user)
+            return redirect('/accounts/profile')
     else:
         form=SignupForm()
     return render(request,'registration/signup.html',{'form':form})
+def profile(request):
+    profile = Profile.objects.get(user=request.user)
+    return render(request,'profile/profile.html',{'profile':profile})
