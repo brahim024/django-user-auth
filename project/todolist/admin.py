5943a471167a4fc955b49a651ddc181c8b41c73f
brahim024 <ifninos168@gmail.com>
2020-08-07 14:37:32 +0200

    add user auth

--- project/todolist/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Post , Category , Apply
-admin.site.register(Post)
-admin.site.register(Category)
-admin.site.register(Apply)
-#Post is a model class
