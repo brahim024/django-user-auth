e5a03d5a7d6811739c7909d8d2aa62f34bb52bfa
brahim024 <ifninos168@gmail.com>
2020-08-07 18:16:18 +0200

    add create profile signal

--- project/accounts/models.py
+++ project/accounts/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 # Create your models here.
 class Profile(models.Model):
     user=models.OneToOneField(User,on_delete=models.CASCADE)
@@ -7,3 +9,10 @@ class Profile(models.Model):
     adress=models.CharField(max_length=30,null=True,blank=True)
     def __str__(self):
         return str(self.user)
+@receiver(post_save, sender=User)
+def my_call(sender,instance,created,**kwargs):
+    if created:
+        Profile.objects.create(
+            user=instance
+        )
+    
