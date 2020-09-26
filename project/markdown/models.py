41339d449f6d2abb07ab30e087234915ada59186
brahim024 <ifninos168@gmail.com>
2020-09-26 02:08:58 +0200

    perform test

--- project/markdown/models.py
+++ project/markdown/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Map:
+  location= models.CharField(max_length=20)
+  destination= models.CharField(max_length=20)
+  destance=models.DecimalField(max_digits=10,decimal_places=2)
+  created=models.DateTimeField(auto_now=True)
+  def __str__(self):
+    return f"im destinate from {self.location} to {self.destination} is {self.destance}"
